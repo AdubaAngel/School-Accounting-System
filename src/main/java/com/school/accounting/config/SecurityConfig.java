279b802a590aee435a8095303a17a89aa7edb644
aduba <adubaangel7@gmail.com>
2026-04-16 23:41:12 -0300

    New imports

--- src/main/java/com/school/accounting/config/SecurityConfig.java
+++ src/main/java/com/school/accounting/config/SecurityConfig.java
@@ -4,22 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.school.accounting.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
-
-    SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        // return your CustomUserDetailsService
-        return userDetailsService;
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-
-    
 }
